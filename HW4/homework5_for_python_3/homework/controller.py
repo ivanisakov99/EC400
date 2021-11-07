90a9d6955e7e4d19b1472525bfc5e3dd8bbc7b5d
Ivan Isakov <60196280+ivanisakov99@users.noreply.github.com>
2021-11-07 15:04:18 -0500

    Update hw5

--- HW4/homework5_for_python_3/homework/controller.py
+++ HW4/homework5_for_python_3/homework/controller.py
@@ -9,7 +9,6 @@ def control(aim_point, current_vel):
     :return: a pystk.Action (set acceleration, brake, steer, drift)
     """
     action = pystk.Action()
-
     """
     Your code here
     Hint: Use action.acceleration (0..1) to change the velocity. Try targeting a target_velocity (e.g. 20).
@@ -17,13 +16,47 @@ def control(aim_point, current_vel):
     Hint: Use action.steer to turn the kart towards the aim_point, clip the steer angle to -1..1
     Hint: You may want to use action.drift=True for wide turns (it will turn faster)
     """
-
-   
-
     
+    # Direction of the red circle
+    direction = aim_point[0]
+    scalingFactor = 4
+
+    # How much do we steer towards the red circle
+    if (direction > 0):
+        steering = min(1, direction * scalingFactor)
+    elif (direction < 0):
+        steering = max(-1, direction * scalingFactor)
+    else:
+        steering = 0
+
+    # Try to maintain a constant velocity
+    if current_vel < 21:
+        action.acceleration = 1
+    else:
+        action.acceleration = 0
+
+    # Skid, if the turn is sharp
+    if (direction < -0.2 or 0.2 < direction):
+        if direction < 0:
+            steering = -1
+        elif direction > 0:
+            steering = 1
+        action.drift = True
+    else:
+        action.drift = False
+
+    # If the red circle is too far left or right, we slow down and readjust the direction of travel
+    if direction < -0.9 or 0.9 < direction:
+        action.drift = False
+        action.brake = True
+
+    # Activate Nitro
+    action.nitro = True
+    
+    # Save the steering angle
+    action.steer = steering
 
     return action
-
 
 
 if __name__ == '__main__':
@@ -34,10 +67,12 @@ if __name__ == '__main__':
         import numpy as np
         pytux = PyTux()
         for t in args.track:
-            steps, how_far = pytux.rollout(t, control, max_frames=1000, verbose=args.verbose)
+            steps, how_far = pytux.rollout(t,
+                                           control,
+                                           max_frames=1000,
+                                           verbose=args.verbose)
             print(steps, how_far)
         pytux.close()
-
 
     parser = ArgumentParser()
     parser.add_argument('track', nargs='+')
